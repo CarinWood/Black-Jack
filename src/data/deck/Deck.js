c4bf4af1d5937dde93579bf8ab31e3ad58c88a9e
Carin Wood <carin.wood.85@gmail.com>
2023-02-19 14:17:30 +0100

    update

---
+++ src/data/deck/Deck.js
@@ -0,0 +1,9 @@
+import Card02 from "../../components/card02/Card02";
+
+export const Deck = [
+    <Card02 suit={"hearts"}/>,
+    <Card02 suit={"spades"}/>,
+    <Card02 suit={"diamonds"}/>,
+    <Card02 suit={"clubs"}/>,
+    
+]
--- src/pages/gameTable/GameTable.js
+++ src/pages/gameTable/GameTable.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import './gameTable.css'
-import UserButtons from "../../components/userButtons/UserButtons";
-import Card02 from "../../components/card02/Card02";
+import { Deck } from "../../data/deck/Deck";
 
 const GameTable = () => {
+  const [countDealerCards, setCountDealerCards] = useState(0)
+  const [firstDealerCard, setFirstDealerCard] = useState(false);
+
+
+   const drawDealerCards = () => {
+    setCountDealerCards(countDealerCards + 1);
+    if(countDealerCards === 1) {
+      setFirstDealerCard(true)
+      console.log(firstDealerCard)
+    }
+  }
+
+  const drawFirstCard = () => {
+    console.log('drawfirstcard function')
+    let card = Deck[0];
+    Deck.splice(0, 1);
+    return card
+  
+  }
+
+
+
   return (
     <div className="game-table">
       <div className="dealers-box">
-        <Card02 suit={"spades"}/>
-      </div>
 
-      <UserButtons />
+        <div className="first-dealer-card">
+          {firstDealerCard === true ? drawFirstCard() : ''}
+        </div>
+     
+      
+      </div>
+ 
+      <div className='user-box'>
+      <button className='stand-btn'>Stand</button>
+      <button className='hit-btn' onClick={()=> drawDealerCards()}>Hit</button>
+    </div>
     </div>
   );
 };

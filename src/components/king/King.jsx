583769ac48d1fac87b71b0ef361a7945d1d89759
Carin Wood <carin.wood.85@gmail.com>
2023-02-20 17:39:35 +0100

    updated king card

--- src/components/king/King.jsx
+++ src/components/king/King.jsx
@@ -1,37 +1,37 @@
 import React from 'react'
 import './king.css'
-import { BsFillSuitHeartFill } from "react-icons/bs";
-import { GiClubs, GiDiamonds, GiSpades } from "react-icons/gi";
+import { GiClubs, GiDiamonds, GiSpades, GiHearts } from "react-icons/gi";
 import { FaChessKing } from "react-icons/fa";
 
 
 const King = ({suit}) => {
   return (
     <div className='card-frame'>
-    <p className={suit === 'hearts' || suit === 'diamonds' ? "first-digit red": "first-digit black"}>K</p>
-       
-          {suit === 'hearts' && <BsFillSuitHeartFill className='heart-suit-small'/>} 
-          {suit === 'clubs' && <GiClubs className='clubs-suit-small'/>}
-          {suit === 'diamonds' && <GiDiamonds className='diamonds-suit-small'/>}
-          {suit === 'spades' && <GiSpades className='spades-suit-small'/>}
+      <p className={suit === 'hearts' || suit === 'diamonds' ? "king-k red": "king-k black"}>K</p>
+         
+            {suit === 'hearts' && <GiHearts className='hearts-suit-small' id="hearts-small"/>} 
+            {suit === 'clubs' && <GiClubs className='clubs-suit-small' id="clubs-small"/>}
+            {suit === 'diamonds' && <GiDiamonds className='diamonds-suit-small' id="diamonds-small"/>}
+            {suit === 'spades' && <GiSpades className='spades-suit-small' id="spades-small"/>}
 
-          {suit === 'hearts' && <BsFillSuitHeartFill className='first-heart'/>}
-          {suit === 'diamonds' && <GiDiamonds className='first-diamond'/>}
-          {suit === 'clubs' && <GiClubs className='first-club'/>}
-          {suit === 'spades' && <FaChessKing className='first-spade'/>}
+            {suit === 'hearts' && <FaChessKing className='first-heart'/>}
+            {suit === 'diamonds' && <FaChessKing className='first-diamond'/>}
+            {suit === 'clubs' && <FaChessKing className='first-club'/>}
+            {suit === 'spades' && <FaChessKing className='first-spade'/>}
             
-          {suit === 'hearts' && <BsFillSuitHeartFill className='second-heart'/>}
-          {suit === 'diamonds' && <GiDiamonds className='second-diamond'/>}
-          {suit === 'clubs' && <GiClubs className='second-club'/>}
-          {suit === 'spades' && <FaChessKing className='second-spade'/>}
+            {suit === 'hearts' && <FaChessKing className='second-heart'/>}
+            {suit === 'diamonds' && <FaChessKing className='second-diamond'/>}
+            {suit === 'clubs' && <FaChessKing className='second-club'/>}
+            {suit === 'spades' && <FaChessKing className='second-spade'/>}
 
-          {suit === 'hearts' && <BsFillSuitHeartFill className='heart-suit-small-2'/>} 
-          {suit === 'clubs' && <GiClubs className='clubs-suit-small-2'/>}
-          {suit === 'diamonds' && <GiDiamonds className='diamonds-suit-small-2'/>}
-          {suit === 'spades' && <GiSpades className='spades-suit-small-2'/>}
-       
-          <p className={suit === 'hearts' || suit === 'diamonds' ? "second-digit red": "second-digit black"}>K</p>
-  </div>
+            {suit === 'hearts' && <GiHearts className='heart-suit-small-2' id="heart-2"/>} 
+            {suit === 'clubs' && <GiClubs className='clubs-suit-small-2' id="king-clubs-2"/>}
+            {suit === 'diamonds' && <GiDiamonds className='diamonds-suit-small-2' id="diamonds-2"/>}
+            {suit === 'spades' && <GiSpades className='spades-suit-small-2' id="spades-2"/>}
+         
+            <p className={suit === 'hearts' || suit === 'diamonds' ? "second-k red": "second-k black"}>K</p>
+    </div>
+   
   )
 }
 

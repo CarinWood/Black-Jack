4f8d74cc024870e780244ae02dde4cee6edc7ffb
Carin Wood <carin.wood.85@gmail.com>
2023-02-22 23:18:44 +0100

    updated heart 3 card

---
+++ src/components/card03/Card03.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './card03.css'
+import { GiClubs, GiDiamonds, GiSpades, GiHearts } from "react-icons/gi";
+
+const Card03 = ({suit}) => {
+  return (
+    
+    <div className='card-frame'>
+      <p className={suit === 'hearts' || suit === 'diamonds' ? "first-digit red": "first-digit black"}>3</p>
+         
+            {suit === 'hearts' && <GiHearts className='hearts-suit-small' id="hearts-small"/>} 
+            {suit === 'clubs' && <GiClubs className='clubs-suit-small' id="clubs-small"/>}
+            {suit === 'diamonds' && <GiDiamonds className='diamonds-suit-small' id="diamonds-small"/>}
+            {suit === 'spades' && <GiSpades className='spades-suit-small' id="spades-small"/>}
+
+            {suit === 'hearts' && <GiHearts className='first-heart-03'/>}
+            {suit === 'diamonds' && <GiDiamonds className='first-diamond'/>}
+            {suit === 'clubs' && <GiClubs className='first-club'/>}
+            {suit === 'spades' && <GiSpades className='first-spade'/>}
+        
+            {suit === 'hearts' && <GiHearts className='middle-heart'/>}
+            {suit === 'diamonds' && <GiDiamonds className='middle-diamond'/>}
+            {suit === 'clubs' && <GiClubs className='middle-club'/>}
+            {suit === 'spades' && <GiSpades className='middle-spade'/>}
+            
+            {suit === 'hearts' && <GiHearts className='second-heart-03'/>}
+            {suit === 'diamonds' && <GiDiamonds id='second-diamond-02'/>}
+            {suit === 'clubs' && <GiClubs id='second-club-02'/>}
+            {suit === 'spades' && <GiSpades id='second-spade-02'/>}
+
+            {suit === 'hearts' && <GiHearts className='heart-suit-small-2' id="heart-2"/>} 
+            {suit === 'clubs' && <GiClubs className='clubs-suit-small-2' id="clubs-2"/>}
+            {suit === 'diamonds' && <GiDiamonds className='diamonds-suit-small-2' id="diamonds-2"/>}
+            {suit === 'spades' && <GiSpades className='spades-suit-small-2' id="spades-2"/>}
+         
+            <p className={suit === 'hearts' || suit === 'diamonds' ? "second-digit red": "second-digit black"}>3</p>
+    </div>
+  )
+}
+
+export default Card03
--- src/data/deck/Deck.js
+++ src/data/deck/Deck.js
@@ -1,26 +1,27 @@
 import Card02 from '../../components/card02/Card02';
 import King from "../../components/king/King";
 import Queen from "../../components/queen/Queen";
+import Card03 from '../../components/card03/Card03';
 
 export const Deck = [
     {
-        face: <Queen suit={"diamonds"}/>,
-        value: 10,
-    },
-    {
-        face:  <Queen suit={"clubs"}/>,
-        value: 10,
-    },
-    {
-        face:  <Queen suit={"hearts"}/>,
-        value: 10,
+        face: <Card03 suit={"hearts"}/>,
+        value: 3,
     },
     {
         face:  <Card02 suit={"hearts"}/>,
+        value: 10,
+    },
+    {
+        face:  <Card03 suit={"hearts"}/>,
+        value: 10,
+    },
+    {
+        face:  <Card03 suit={"spades"}/>,
         value: 2,
     },
     {
-        face:  <Card02 suit={"hearts"}/>,
+        face:  <Queen suit={"spades"}/>,
         value: 2,
     },
     {

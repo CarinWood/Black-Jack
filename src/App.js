c4d68da064311b66da686edf0207aa6ac7c9e99f
Carin Wood <carin.wood.85@gmail.com>
2023-03-27 19:02:48 +0200

    updated background to black

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import GameTable from "./pages/gameTable/GameTable";
 import StartPage from "./pages/startPage/StartPage";
 import { Routes, Route } from "react-router-dom";
 import Faq from "./pages/faqPage/Faq";
+import './index.css'
 
 function App() {
   return (
-    <>
+    <div className="app">
     <Routes>
     <Route path="/" element={<StartPage />}/>
     <Route path="/play" element={<GameTable />}/>
     <Route path="/faq" element={<Faq/>}/>
   
     </Routes>
-    </>
+    </div>
   );
 }
 

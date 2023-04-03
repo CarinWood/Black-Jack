26af27dfba57ba40f9f8d5fae1e98b2532fc5d6e
Carin Wood <carin.wood.85@gmail.com>
2023-04-03 22:10:29 +0200

    added balance under place-your-bet-text

--- src/pages/gameTable/GameTable.js
+++ src/pages/gameTable/GameTable.js
@@ -1134,7 +1134,7 @@ function shuffleArray(array) {
       {isBust && <p className="bust-text">Bust!</p>}
       {standOff && <p className="bust-text standoff-text">Stand off!</p>}
       {renderUserSum()}
-      <p className= {greenBetted || redBetted || blueBetted || blackBetted ? "place-bet-text hidden": "place-bet-text"}>Place Your Bet</p>
+      <p className= {greenBetted || redBetted || blueBetted || blackBetted ? "place-bet-text hidden": "place-bet-text"}>Place Your Bet <span className="balance">Balance: $ {currentMoney}</span></p>
       <div className={greenBetted || redBetted || blueBetted || blackBetted ? 'user-box' : 'user-box hidden'}>
       <button className='stand-btn' onClick={()=> drawDealerCards()}>Stand</button>
       <button className='hit-btn' onClick={() => drawUserCards()}>Hit</button>
